d752aa9a0903d401a3921f84c9f99d61616c60ce
Adam Bargteil <adamb@cs.utah.edu>
2015-04-25 20:06:43 -0600

    okay now the outlier removal doesn't cause things to explode

--- particle.h
+++ particle.h
@@ -11,10 +11,15 @@ public:
 	goalPosition, 
 	goalVelocity;
   double mass; 
-  double totalweight;
-  size_t numClusters;
+  double totalweight; // only used for initialization
+  size_t numClusters; // only used to determine singelton particles
   bool outsideSomeMovingPlane;
-  std::vector<int> clusters;
+  std::vector<int> clusters; 
+  Particle() {};
+  Particle(const Particle &p) : position(p.position), velocity(p.velocity), restPosition(p.restPosition),
+	oldPosition(p.oldPosition), goalPosition(p.goalPosition), goalVelocity(p.goalVelocity), mass(p.mass),
+	totalweight(p.totalweight), numClusters(p.numClusters), outsideSomeMovingPlane(p.outsideSomeMovingPlane),
+	clusters(p.clusters){};
 };
 
 class Cluster {
